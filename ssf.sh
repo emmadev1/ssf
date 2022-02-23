1efb95bdca94351321fffe0963695cb92d883271
Emma <99513625+emmadev1@users.noreply.github.com>
2022-02-23 13:05:02 +0100

    Add files via upload

---
+++ ssf.sh
@@ -0,0 +1,14 @@
+iptables -N TCP
+iptables -N UDP
+iptables -P OUTPUT ACCEPT
+iptables -P INPUT DROP
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP  
+iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT 
+iptables -A INPUT -p udp -m conntrack --ctstate INVALID -j DROP
+iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
+iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+iptables-save -f /etc/iptables/iptables.rules
