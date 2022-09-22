4e616370028c102ce2f0decdafee585a3cb2673c
Emma <99513625+emmadev1@users.noreply.github.com>
2022-09-22 09:56:38 +0200

    Update ssf.sh

--- ssf.sh
+++ ssf.sh
@@ -6,7 +6,7 @@ iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP  
 iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT 
-iptables -A INPUT -p udp -m conntrack --ctstate INVALID -j DROP
+iptables -A INPUT -p udp -m conntrack --ctstate INVALID -j UDP
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
 iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
 iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
